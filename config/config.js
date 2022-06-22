47f9e59651af3d0a63958634ccdaf01121a0d0b3
Acássio Moura Vilela <maestro.etmaxx@gmail.com>
2022-06-22 16:07:52 -0300

    modificando urls

--- config/config.js
+++ config/config.js
@@ -1,3 +1,5 @@
-//const urlApi= "http://localhost:3000";
+/* const urlApi= "http://localhost:3000";
+const urlSite = "http://127.0.0.1:5500/pages/product/product.html"; */
+
 const urlApi= "https://json-server-web-store.herokuapp.com"
-const urlSite = "http://127.0.0.1:5500/pages/product/product.html";
+const urlSite = "https://mourajrs.github.io/web-store/pages/product/product.html";
