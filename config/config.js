54dca6da8e074795c4cd9de5e1c8534d85cb9c5e
Acássio Moura Vilela <48871449+mourajrs@users.noreply.github.com>
2022-06-22 15:15:53 -0300

    Adicionando url api

--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
 //const urlApi= "http://localhost:3000";
-const urlApi=process.env.URLAPI;
+const urlApi= "https://json-server-web-store.herokuapp.com"
 const urlSite = "http://127.0.0.1:5500/pages/product/product.html";
