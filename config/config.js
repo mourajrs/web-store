c45ad32dd1ad39a6ec1768d7ac360e305ef7228e
Acássio Moura Vilela <48871449+mourajrs@users.noreply.github.com>
2022-06-22 15:12:49 -0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,2 +1,3 @@
-const urlApi= "http://localhost:3000";
+//const urlApi= "http://localhost:3000";
+const urlApi=process.env.URLAPI;
 const urlSite = "http://127.0.0.1:5500/pages/product/product.html";
